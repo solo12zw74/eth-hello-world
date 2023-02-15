7eb355279ecf811a7a090efad1551b06a544865c
solo12zw74 <solo12zw74@gmail.com>
2023-02-14 21:18:23 -0600

    Create the HelloWorld contract

---
+++ contracts/HelloWorld.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.17;
+
+contract HelloWorld {
+    function SayHello() public pure returns (string memory) {
+        return "Hello, world!";
+    }
+}
