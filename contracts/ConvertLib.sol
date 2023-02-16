3b48332f7cc54f04c012a0cb325db0f23b111ab3
solo12zw74 <solo12zw74@gmail.com>
2023-02-15 21:19:46 -0600

    Update solidity ver spec to make remove error рште

--- contracts/ConvertLib.sol
+++ contracts/ConvertLib.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.5.16;
+pragma solidity >=0.5.16;
 
 library ConvertLib{
 	function convert(uint amount,uint conversionRate) public pure returns (uint convertedAmount)
--- contracts/MetaCoin.sol
+++ contracts/MetaCoin.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.5.16;
+pragma solidity >=0.5.16;
 
 import "./ConvertLib.sol";
 
